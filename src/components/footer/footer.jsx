69f066586a171d1d6e6affad7fc02aee38a728dc
Sergio Inzunza <sergio@Toolss-MacBook-Pro.local>
2021-01-31 11:57:38 -0700

    first class commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <center>This is the footer of my site</center>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"; // imrc
+
+// cc
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Menu here later!</h1>
+        <hr></hr>
+        some text on the bottom of the navBar comp
+      </div>
+    );
+  }
+}
+
+export default NavBar;
